6b6f5349eb6155d01f19c6fa4a404b3a3a79e3c0
SilentCicero <silentcicero@outlook.com>
2016-05-02 13:47:26 -0400

    Fix progress bar multiple el

--- progress-bar.js
+++ progress-bar.js
@@ -11,11 +11,8 @@ const ProgressBar = function(props) {
                + (props.bsStyle && "progress-bar-" + props.bsStyle + " " || ' ');
 
   return yo`
-    <span class="">
-      ${Children(arguments)}
-    </span>
     <div class="progress">
-      ${Children(arguments) && Children(arguments) || yo`
+      ${arguments[0] && Children(arguments) || yo`
         <div class="${className}" role="progressbar" aria-valuenow="${props.now && props.now || 0}" aria-valuemin="${props.min && props.min || 0}" aria-valuemax="${props.max && props.max || 100}" style="width: ${props.now && props.now || 0}%;">
           ${(props.label && props.label || '')}
         </div>
